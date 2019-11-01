fdc65646ec1c1ef772671305a7cb2376e6ecc361
Clare Williams <clarewilliams7@sky.com>
2019-11-01 15:45:00 +0000

    removed paginate from index file

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -5,7 +5,7 @@ class PinsController < ApplicationController
   # GET /pins
   # GET /pins.json
   def index
-    @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 6)
+    @pins = Pin.all.order("created_at DESC")
   end
 
   # GET /pins/1
